de1195b1310b4720898f5509fbabe52444b6e1b9
liutao <alex.liutao@outlook.com>
2018-09-04 15:53:50 +0800

    更新

--- pair.go
+++ pair.go
@@ -1,6 +1,9 @@
 package cmap
 
 import (
+	"bytes"
+	"fmt"
+	"sync/atomic"
 	"unsafe"
 )
 
@@ -31,6 +34,18 @@ type Pair interface {
 	String() string
 }
 
+func newPair(key string, element interface{}) (Pair, error) {
+	p := &pair{
+		key:  key,
+		hash: hash(key),
+	}
+	if element == nil {
+		return nil, newIllegalParameterError("element is nil")
+	}
+	p.element = unsafe.Pointer(&element)
+	return p, nil
+}
+
 type pair struct {
 	key     string
 	hash    uint64
@@ -39,10 +54,23 @@ type pair struct {
 }
 
 func (p *pair) Next() Pair {
-	return nil
+	pointer := atomic.LoadPointer(&p.next)
+	if pointer == nil {
+		return nil
+	}
+	return (*pair)(pointer)
 }
 
 func (p *pair) SetNext(nextPair Pair) error {
+	if nextPair == nil {
+		atomic.StorePointer(&p.next, nil)
+		return nil
+	}
+	pp, ok := nextPair.(*pair)
+	if !ok {
+		return newIllegalPairTypeError(nextPair)
+	}
+	atomic.StorePointer(&p.next, unsafe.Pointer(pp))
 	return nil
 }
 
@@ -51,33 +79,58 @@ func (p *pair) Hash() uint64 {
 }
 
 func (p *pair) Element() interface{} {
-	return nil
+	pointer := atomic.LoadPointer(&p.element)
+	if pointer == nil {
+		return nil
+	}
+	return *(*interface{})(pointer)
 }
 
 func (p *pair) SetElement(element interface{}) error {
+	if element == nil {
+		return newIllegalParameterError("element is nil")
+	}
+	atomic.StorePointer(&p.element, unsafe.Pointer(&element))
 	return nil
 }
 
 func (p *pair) Copy() Pair {
-	return nil
+	pCopy, _ := newPair(p.Key(), p.Element())
+	return pCopy
 }
 
 func (p *pair) String() string {
-	return ""
+	return p.genString(false)
 }
 
 func (p *pair) Key() string {
 	return p.key
 }
 
-func newPair(key string, element interface{}) (Pair, error) {
-	p := &pair{
-		key:  key,
-		hash: hash(key),
+// genString 用于生成并返回当前键-元素对的字符串形式。
+func (p *pair) genString(nextDetail bool) string {
+	var buf bytes.Buffer
+	buf.WriteString("pair{key:")
+	buf.WriteString(p.Key())
+	buf.WriteString(", hash:")
+	buf.WriteString(fmt.Sprintf("%d", p.Hash()))
+	buf.WriteString(", element:")
+	buf.WriteString(fmt.Sprintf("%+v", p.Element()))
+	if nextDetail {
+		buf.WriteString(", next:")
+		if next := p.Next(); next != nil {
+			if npp, ok := next.(*pair); ok {
+				buf.WriteString(npp.genString(nextDetail))
+			} else {
+				buf.WriteString("<ignore>")
+			}
+		}
+	} else {
+		buf.WriteString(", nextKey:")
+		if next := p.Next(); next != nil {
+			buf.WriteString(next.Key())
+		}
 	}
-	if element == nil {
-		return nil, newIllegalParameterError("elemem")
-	}
-	p.element = unsafe.Pointer(&element)
-	return nil, nil
+	buf.WriteString("}")
+	return buf.String()
 }
